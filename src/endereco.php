fb94963fff3861d28e8ed148dbc53062cf166982
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2021-08-19 16:26:12 -0300

    Add files via upload

---
+++ src/endereco.php
@@ -0,0 +1,44 @@
+<?php
+
+
+class Endereco
+{
+
+
+  private string $cidade;
+  private string $bairro;
+  private string $rua;
+  private string $numero;
+
+public function __construct(string $cidade,string $bairro,string $rua,string $numero)
+  {
+    $this->cidade = $cidade;
+    $this->bairro = $bairro;
+    $this->rua = $rua;
+    $this->numero = $numero;
+  }
+
+  public function getcidade():string
+  {
+    return $this->cidade;
+  }
+
+  public function getbairro():string
+  {
+    return $this->bairro;
+  }
+
+  public function getrua():string
+  {
+    return $this->rua;
+  }
+
+  public function getnumero():string
+  {
+    return $this->numero;
+  }
+}
+
+
+
+ ?>
