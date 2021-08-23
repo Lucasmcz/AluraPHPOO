be9e075c95188b8f840a0138a09705d0aa35a21a
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2021-08-23 19:26:31 -0300

    Update and rename src/Modelo/funcionario.php to src/Modelo/fu/Funcionario.php

--- src/Modelo/fu/Funcionario.php
+++ src/Modelo/fu/Funcionario.php
@@ -1,23 +1,24 @@
 <?php
-namespace Alura\Banco\modelo;
 
-require_once 'pessoa.php';
+namespace Alura\Banco\Modelo;
+require_once "Pessoa.php";//Gambiarra
+
 class Funcionario extends Pessoa
 {
 
   private string $cargo;
-  private $endereco;
 
-  public function __construct(string $nome , Cpf $cpf , Endereco $endereco)
+
+  public function __construct(string $nome , Cpf $cpf ,string $cargo)
   {
     parent::__construct($nome,$cpf);
-    $this->endereco = $endereco;
+    $this->cargo = $cargo;
   }
 
 
-  public function getcargo(string $cargo)
+  public function getcargo(string $cargo):string
   {
-    return $this->nome;
+    return $this->cargo;
   }
   public function alteranome(string $nome):void
   {
