4fc74ffeca5ce360a3d0cc2eb09511b87e10642d
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2021-08-24 10:59:53 -0300

    Update test-saque.php

--- src/Modelo/Conta/test-saque.php
+++ src/Modelo/Conta/test-saque.php
@@ -7,26 +7,38 @@ require_once 'Titular.php';
 require_once '/home/gestor/Área de Trabalho/Formação PHP/1LinguagemPHP/OOPHP/src/Modelo/CPF.php';
 require_once 'Conta.php';
 require_once 'conta_poupanca.php';
+require_once 'conta_corrente.php';
 
 use Alura\Banco\modelo\Cpf;
 use Alura\Banco\modelo\Endereco;
 use Alura\Banco\modelo\Funcionario;
 use Alura\Banco\modelo\Pessoa;
+use Alura\Banco\modelo\Conta\Corrente;
 
-$conta = new Conta
-(new Titular(
-new Cpf('124.214.144-42'),
-"Kaneki",
-new Endereco('Tokyo','Karasuno','Ohayo','993'),
-)
+$conta = new Corrente(
+    new Titular(
+    new Cpf('124.214.144-42'),
+    "Kaneki",
+    new Endereco('Tokyo','Karasuno','Ohayo','993'),
+    )
 );
-
-
 $conta->depositar(500);
 $conta->sacar(100);
 
 
-echo $conta->recuperarSaldo();
+$conta2 = new Poupanca(
+    new Titular(
+    new Cpf('124.214.144-42'),
+    "Kira the god of new world",
+    new Endereco('Tokyo','Karasuno','Ohayo','1007'),
+      )
+);
+
+$conta2->depositar(500);
+$conta2->sacar(100);
+
+echo $conta->recuperarSaldo() .PHP_EOL;
+echo $conta2->recuperarSaldo();
 
 
  ?>
