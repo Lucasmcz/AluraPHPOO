4d840987dc4fc0759e5047b397d27e3e34dae836
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2021-08-24 16:42:35 -0300

    Update Bonificacoes.php

--- src/Services/Bonificacoes.php
+++ src/Services/Bonificacoes.php
@@ -6,7 +6,7 @@ require_once 'controladorboni.php';
 require_once '/home/gestor/Área de Trabalho/Formação PHP/1LinguagemPHP/OOPHP/src/Modelo/Funcionario/Gerente.php';
 require_once '/home/gestor/Área de Trabalho/Formação PHP/1LinguagemPHP/OOPHP/src/Modelo/Funcionario/Diretor.php';
 require_once '/home/gestor/Área de Trabalho/Formação PHP/1LinguagemPHP/OOPHP/src/Modelo/Funcionario/Dev.php';
-//Chamadas do Use
+
 use Alura\Banco\Modelo\CPF;
 use Alura\Banco\Services\CrontoladorBonificacoes;
 use Alura\Banco\Modelo\Funcionario\Diretor;
@@ -18,13 +18,13 @@ use Alura\Banco\Modelo\Funcionario\Desenvolvedor;
 
 
 $outro_funcionario = new Gerente('Yelena', new Cpf('123.456.230-13'), 'Gerente' , 3000);//3K
-$finally = new Diretor('Baiano',new Cpf('213.323.213-45'),'Diretor',1000);//2K
+$finally = new Diretor('Baiano',new Cpf('213.323.213-45'),'Diretor',5000);//2K
 $our_first_major_victory = new Desenvolvedor('Yelena Fan',new Cpf('123.124.130-14'),'Dev PHP',1000);//
-
+$our_first_major_victory->sobenivel();
 
 $controlador = new CrontoladorBonificacoes();
 
-//var_dump($controlador);
+
 $controlador->adicionar_bonificacoes($outro_funcionario);
 $controlador->adicionar_bonificacoes($finally);
 $controlador->adicionar_bonificacoes($our_first_major_victory);
